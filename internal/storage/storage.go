17a239db581d1b703d65efdd98876fd37679acdb
Glebkin Kirill <glebkinkirill@gmail.com>
2026-02-03 22:34:00 +0300

    feat: Добавлен интерфейс для взаимодействия с хранилищем

---
+++ internal/storage/storage.go
@@ -0,0 +1,19 @@
+package storage
+
+import (
+	"context"
+
+	"github.com/C-4KE/simple-posts-service/graph/model"
+	"github.com/google/uuid"
+)
+
+type Storage interface {
+	AddPost(ctx context.Context, newPost *model.PostInput) (*model.Post, error)
+	GetPost(ctx context.Context, postID int64) (*model.Post, error)
+	GetAllPosts(ctx context.Context) ([]*model.Post, error)
+	UpdateCommentsEnabled(ctx context.Context, postID int64, authorID uuid.UUID, newCommentsEnabled bool) (*model.Post, error)
+
+	AddComment(ctx context.Context, newComment *model.CommentInput) (*model.Comment, error)
+	GetCommentPath(ctx context.Context, postID int64, parentID int64) (string, error)
+	GetCommentsBranch(ctx context.Context, postID int64, path string) ([]*model.Comment, error)
+}
