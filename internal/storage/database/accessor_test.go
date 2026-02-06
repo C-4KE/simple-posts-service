d3e7e0af871c4bfa8c57db6d87064911aa52fdd7
Glebkin Kirill <glebkinkirill@gmail.com>
2026-02-06 17:27:48 +0300

    feat: Доделаны тесты по взаимодействию с постами в БД

--- internal/storage/database/accessor_test.go
+++ internal/storage/database/accessor_test.go
@@ -66,94 +66,145 @@ func TestAddPost(t *testing.T) {
 		}, createdPost)
 	})
 
-	// t.Run("Successful Update CommentsEnabled", func(t *testing.T) {
-	// 	updatedPost, err := mockAccessor.UpdateCommentsEnabled(ctx, 0, authorID, false)
-	// 	assertions.Nil(err)
-	// 	assertions.NotNil(updatedPost)
-	// 	assertions.Equal(updatedPost.CommentsEnabled, false)
-	// })
+	t.Run("Successful Update CommentsEnabled", func(t *testing.T) {
+		mockAccessor, mock := getMockAccessor(t)
+		defer mockAccessor.CloseStorage()
 
-	// t.Run("Unsuccessful Update CommentsEnabled Incorrect PostID", func(t *testing.T) {
-	// 	updatedPost, err := mockAccessor.UpdateCommentsEnabled(ctx, -1, authorID, false)
-	// 	assertions.NotNil(err)
-	// 	assertions.Nil(updatedPost)
-	// })
+		mock.ExpectQuery(`SELECT post_id, author_id
+						FROM posts
+						WHERE post_id = \$1 AND author_id = \$2`).
+			WithArgs(int64(0), authorID).
+			WillReturnRows(sqlmock.NewRows([]string{"post_id", "author_id"}).AddRow(int64(0), authorID))
 
-	// t.Run("Unsuccessful Update CommentsEnabled Incorrect AuthorID", func(t *testing.T) {
-	// 	updatedPost, err := mockAccessor.UpdateCommentsEnabled(ctx, 0, uuid.New(), false)
-	// 	assertions.NotNil(err)
-	// 	assertions.Nil(updatedPost)
-	// })
+		mock.ExpectQuery(`UPDATE posts SET comments_enabled = \$1
+						WHERE post_id = \$2
+						RETURNING post_id, author_id, title, text, create_date, comments_enabled`).
+			WithArgs(int64(0), false).
+			WillReturnRows(sqlmock.
+				NewRows([]string{"post_id", "author_id", "title", "text", "create_date", "comments_enabled"}).
+				AddRow(int64(0), authorID, "Test Title", "Test Text", time.Now(), false))
 
-	// t.Run("Successful Get Post", func(t *testing.T) {
-	// 	existingPost := &model.PostInput{
-	// 		AuthorID:        authorID,
-	// 		Title:           "Test Title",
-	// 		Text:            "Test Text",
-	// 		CommentsEnabled: false,
-	// 	}
+		updatedPost, err := mockAccessor.UpdateCommentsEnabled(ctx, 0, authorID, false)
+		assertions.Nil(err)
+		assertions.NotNil(updatedPost)
+		assertions.Equal(updatedPost.CommentsEnabled, false)
+	})
 
-	// 	post, err := mockAccessor.GetPost(ctx, 0)
-	// 	assertions.Nil(err)
-	// 	assertions.Equal(&model.Post{
-	// 		ID:              0,
-	// 		AuthorID:        existingPost.AuthorID,
-	// 		Title:           existingPost.Title,
-	// 		Text:            existingPost.Text,
-	// 		CommentsEnabled: existingPost.CommentsEnabled,
-	// 		CreateDate:      post.CreateDate,
-	// 	}, post)
-	// })
+	t.Run("Unsuccessful Update CommentsEnabled Incorrect PostID", func(t *testing.T) {
+		mockAccessor, mock := getMockAccessor(t)
+		defer mockAccessor.CloseStorage()
 
-	// t.Run("Unsuccessful Get Post Incorrect PostID", func(t *testing.T) {
-	// 	post, err := mockAccessor.GetPost(ctx, -1)
-	// 	assertions.NotNil(err)
-	// 	assertions.Nil(post)
-	// })
+		mock.ExpectQuery(`SELECT post_id, author_id
+						FROM posts
+						WHERE post_id = \$1 AND author_id = \$2`).
+			WithArgs(int64(0), authorID)
 
-	// t.Run("Successful Another Post", func(t *testing.T) {
-	// 	newPost := &model.PostInput{
-	// 		AuthorID:        authorID,
-	// 		Title:           "Test Title",
-	// 		Text:            "Test Text",
-	// 		CommentsEnabled: true,
-	// 	}
+		err := mock.ExpectationsWereMet()
+		assertions.NotNil(err)
 
-	// 	createdPost, err := mockAccessor.AddPost(ctx, newPost)
-	// 	assertions.Nil(err)
-	// 	assertions.NotNil(createdPost)
-	// 	assertions.Equal(&model.Post{
-	// 		ID:              1,
-	// 		AuthorID:        newPost.AuthorID,
-	// 		Title:           newPost.Title,
-	// 		Text:            newPost.Text,
-	// 		CommentsEnabled: newPost.CommentsEnabled,
-	// 		CreateDate:      createdPost.CreateDate,
-	// 	}, createdPost)
-	// })
+		updatedPost, err := mockAccessor.UpdateCommentsEnabled(ctx, -1, authorID, false)
+		assertions.NotNil(err)
+		assertions.Nil(updatedPost)
+	})
 
-	// t.Run("Successful Get All Posts", func(t *testing.T) {
-	// 	posts, err := mockAccessor.GetAllPosts(ctx)
-	// 	assertions.Nil(err)
-	// 	assertions.Equal([]*model.Post{
-	// 		{
-	// 			ID:              0,
-	// 			AuthorID:        authorID,
-	// 			Title:           "Test Title",
-	// 			Text:            "Test Text",
-	// 			CommentsEnabled: false,
-	// 			CreateDate:      posts[0].CreateDate,
-	// 		},
-	// 		{
-	// 			ID:              1,
-	// 			AuthorID:        authorID,
-	// 			Title:           "Test Title",
-	// 			Text:            "Test Text",
-	// 			CommentsEnabled: true,
-	// 			CreateDate:      posts[1].CreateDate,
-	// 		},
-	// 	}, posts)
-	// })
+	t.Run("Unsuccessful Update CommentsEnabled Incorrect AuthorID", func(t *testing.T) {
+		mockAccessor, mock := getMockAccessor(t)
+		defer mockAccessor.CloseStorage()
+
+		incorrectAuthorID := uuid.New()
+		mock.ExpectQuery(`SELECT post_id, author_id
+						FROM posts
+						WHERE post_id = \$1 AND author_id = \$2`).
+			WithArgs(int64(0), authorID)
+
+		err := mock.ExpectationsWereMet()
+		assertions.NotNil(err)
+
+		updatedPost, err := mockAccessor.UpdateCommentsEnabled(ctx, 0, incorrectAuthorID, false)
+		assertions.NotNil(err)
+		assertions.Nil(updatedPost)
+	})
+
+	t.Run("Successful Get Post", func(t *testing.T) {
+		mockAccessor, mock := getMockAccessor(t)
+		defer mockAccessor.CloseStorage()
+
+		existingPost := &model.PostInput{
+			AuthorID:        authorID,
+			Title:           "Test Title",
+			Text:            "Test Text",
+			CommentsEnabled: false,
+		}
+
+		mock.ExpectQuery(`SELECT post_id, author_id, title, text, create_date, comments_enabled
+						FROM posts
+						WHERE post_id = \$1`).
+			WithArgs(int64(0)).
+			WillReturnRows(sqlmock.
+				NewRows([]string{"post_id", "author_id", "title", "text", "create_date", "comments_enabled"}).
+				AddRow(int64(0), authorID, "Test Title", "Test Text", time.Now(), false))
+
+		post, err := mockAccessor.GetPost(ctx, 0)
+		assertions.Nil(err)
+		assertions.Equal(&model.Post{
+			ID:              0,
+			AuthorID:        existingPost.AuthorID,
+			Title:           existingPost.Title,
+			Text:            existingPost.Text,
+			CommentsEnabled: existingPost.CommentsEnabled,
+			CreateDate:      post.CreateDate,
+		}, post)
+	})
+
+	t.Run("Unsuccessful Get Post Incorrect PostID", func(t *testing.T) {
+		mockAccessor, mock := getMockAccessor(t)
+		defer mockAccessor.CloseStorage()
+
+		mock.ExpectQuery(`SELECT post_id, author_id, title, text, create_date, comments_enabled
+						FROM posts
+						WHERE post_id = \$1`).
+			WithArgs(int64(-1))
+
+		err := mock.ExpectationsWereMet()
+		assertions.NotNil(err)
+
+		post, err := mockAccessor.GetPost(ctx, -1)
+		assertions.NotNil(err)
+		assertions.Nil(post)
+	})
+
+	t.Run("Successful Get All Posts", func(t *testing.T) {
+		mockAccessor, mock := getMockAccessor(t)
+		defer mockAccessor.CloseStorage()
+
+		mock.ExpectQuery(`SELECT post_id, author_id, title, text, create_date, comments_enabled
+						FROM posts`).
+			WillReturnRows(sqlmock.
+				NewRows([]string{"post_id", "author_id", "title", "text", "create_date", "comments_enabled"}).
+				AddRow(int64(0), authorID, "Test Title", "Test Text", time.Now(), false).
+				AddRow(int64(1), authorID, "Test Title", "Test Text", time.Now(), true))
+
+		posts, err := mockAccessor.GetAllPosts(ctx)
+		assertions.Nil(err)
+		assertions.Equal([]*model.Post{
+			{
+				ID:              0,
+				AuthorID:        authorID,
+				Title:           "Test Title",
+				Text:            "Test Text",
+				CommentsEnabled: false,
+				CreateDate:      posts[0].CreateDate,
+			},
+			{
+				ID:              1,
+				AuthorID:        authorID,
+				Title:           "Test Title",
+				Text:            "Test Text",
+				CommentsEnabled: true,
+				CreateDate:      posts[1].CreateDate,
+			},
+		}, posts)
+	})
 
 	// t.Run("Successful Add Comment", func(t *testing.T) {
 	// 	newComment := &model.CommentInput{
